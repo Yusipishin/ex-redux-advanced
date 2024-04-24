0124c0088f00f927c57a5afed950af4c30e75fa5
Alexey <alexey.yus@mail.ru>
2024-04-24 18:03:20 +0300

    fix typescript error in extraReducers

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -19,19 +19,20 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {},
-    extraReducers: {
-        [fetchUsers.fulfilled.type]: (state: UserState, action: PayloadAction<IUser[]>) => {
-            state.isLoading = false;
-            state.error = ''
-            state.users = action.payload;
-        },
-        [fetchUsers.pending.type]: (state: UserState) => {
-            state.isLoading = true;
-        },
-        [fetchUsers.rejected.type]: (state: UserState,  action: PayloadAction<string>) => {
-            state.isLoading = false;
-            state.error = action.payload
-        },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUsers.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = ''
+                state.users = action.payload;
+            })
+            .addCase(fetchUsers.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchUsers.rejected, (state,  action) => {
+                state.isLoading = false;
+                state.error = action.payload
+            })
     }
 })
 
